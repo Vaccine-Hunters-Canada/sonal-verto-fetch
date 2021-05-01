6a741828437e4d9da60ddae4cacb3c432b4ab60e
Sonal Ranjit <sonal.ranjit3@gmail.com>
2021-05-01 19:13:35 -0400

    updated output to show groups as well

--- get_available_slots.py
+++ get_available_slots.py
@@ -17,6 +17,7 @@ def get_open_slots():
     print("----------------------------------------")
     print("")
     print(datetime.now().strftime("%Y-%d-%mT%H:%M:%S") + " : Checking for new open slots")
+
     with open(os.path.join(DATA_DIR, LAST_RUN_JSON)) as last_run_json_file:
         last_run_json = json.load(last_run_json_file)
 
@@ -28,53 +29,60 @@ def get_open_slots():
 
     for clinic in data:
         clinic_name = data[clinic]['name']
-        
         total_open_slots = 0
         clinic_data = {}
         clinic_data["name"] = clinic_name
         clinic_data["open_slots"] = {}
         clinic_open_slots = {}
+        clinic_open_slots_group = {}
+
         for group in data[clinic]['availabilities']:
             open_slots = 0
+            clinic_open_slots_group_open_slots = {}
             for slot in data[clinic]['availabilities'][group]:
                 open_slots = data[clinic]['availabilities'][group][slot]
                 trimmed_datetime = slot.split("T")[0]
-                if trimmed_datetime in clinic_open_slots:
-                    clinic_open_slots[trimmed_datetime] = clinic_open_slots[trimmed_datetime] + open_slots
-                else:
-                    clinic_open_slots[trimmed_datetime] = open_slots
+                clinic_open_slots_group_open_slots[trimmed_datetime] = open_slots
                 total_open_slots += open_slots
+            clinic_open_slots_group[group] = clinic_open_slots_group_open_slots
 
-        clinic_data["open_slots"] = clinic_open_slots
+        clinic_data["open_slots"] = clinic_open_slots_group
         clinic_data["total_open_slots"] = total_open_slots
         parsed_data[clinic] = clinic_data
-        
+
     date_time_obj = datetime.now()
     output_json_file = date_time_obj.strftime("%Y-%d-%m_%H-%M-%S") + '.json'
+
+    with open(os.path.join(DATA_DIR, output_json_file), 'w') as output_json:
+        json.dump(parsed_data, output_json, indent=4)
 
     for data in parsed_data:
         if (data != 'date'):
             total_open_slots = parsed_data[data]['total_open_slots']
             last_run_total_open_slots = last_run_json[data]['total_open_slots']
-            total_diff_since_last_run = total_open_slots - last_run_json[data]['total_open_slots']
+            total_diff_since_last_run = total_open_slots - last_run_json[data]['total_open_slots'] 
             parsed_data[data]['change_since_last_fetch'] = total_open_slots - last_run_total_open_slots
 
             print("")
             print("Clinic: {}".format(parsed_data[data]['name']))
-            print("------------------------------")
+            print("---------------------------------")
 
-            for slot in parsed_data[data]['open_slots']:
-                current_slots = parsed_data[data]['open_slots'][slot]
-                if (current_slots > 0):
-                    diff_since_last_run_slot = current_slots - last_run_json[data]['open_slots'][slot]
-                    print("{} : {} ({:+})".format(slot, current_slots, diff_since_last_run_slot))
+            for group in parsed_data[data]['open_slots']:
+                group_summary_msg = "Group: {}\n".format(group)
+                are_slots_available = False
+                for date in parsed_data[data]['open_slots'][group]:
+                    available_slots = parsed_data[data]['open_slots'][group][date]
+                    if (available_slots > 0):
+                        are_slots_available = True
+                        diff_since_last_run_slot = available_slots - last_run_json[data]['open_slots'][group][date]
+                        group_summary_msg += "{} : {} ({:+})\n".format(date, available_slots, diff_since_last_run_slot)
+                if are_slots_available:
+                    print(group_summary_msg)
 
             print("Total open slots for {} : {}".format(parsed_data[data]['name'], total_open_slots))
             print("Change since last fetch for {} : {:+}".format(parsed_data[data]['name'], total_diff_since_last_run))
             print("")
 
-    with open(os.path.join(DATA_DIR, output_json_file), 'w') as output_json:
-        json.dump(parsed_data, output_json, indent=4)
     LAST_RUN_JSON = output_json_file
 
 def on_exit_handler():
@@ -86,7 +94,6 @@ if __name__ == '__main__':
     LAST_RUN_JSON = 'last-run.json'
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     DATA_DIR = ROOT_DIR + '/data/'
-
 
     atexit.register(on_exit_handler)
     get_open_slots()
