2a2afbe13f1c56ac173ccf0b0ee30ed8d4ac5a42
Sonal Ranjit <sonal.ranjit3@gmail.com>
2021-05-02 16:36:01 -0400

    Updated output json structure and update README

--- README.md
+++ README.md
@@ -25,6 +25,79 @@ python get_available_slots.py
 You can see an example output of the script to stdout in the file [`example_output.txt`](example_output.txt)
 
 Example output of the fetched data is in json format with the timestamp as the filename, there is an example [`last-run.json`](data/last-run.json).
+```----------------------------------------
+
+2021-02-05T16:33:02 : Checking for new open slots
+
+Clinic: Regent Park 40 Oaks
+---------------------------------
+Total open slots for Regent Park 40 Oaks : 0
+Change since last fetch for Regent Park 40 Oaks : +0
+
+
+Clinic: St. James Town Wellesley Community Centre (WCC)
+---------------------------------
+Group: IndigenousAdults
+2021-05-03 : 1 (+0)
+
+Total open slots for St. James Town Wellesley Community Centre (WCC) : 1
+Change since last fetch for St. James Town Wellesley Community Centre (WCC) : +0
+
+
+Clinic: Ryerson University
+---------------------------------
+Total open slots for Ryerson University : 0
+Change since last fetch for Ryerson University : +0
+
+
+Clinic: St. Michael’s Hospital
+---------------------------------
+Group: High
+2021-05-04 : 7 (+0)
+2021-05-05 : 9 (+0)
+2021-05-06 : 7 (+0)
+2021-05-07 : 7 (+0)
+2021-05-08 : 5 (+0)
+
+Group: Special
+2021-05-08 : 1 (+0)
+
+Total open slots for St. Michael’s Hospital : 36
+Change since last fetch for St. Michael’s Hospital : +0
+
+
+Clinic: St. Joseph’s Health Centre
+---------------------------------
+Group: IndigenousAdults
+2021-05-03 : 1 (+0)
+
+Group: High
+2021-05-05 : 9 (+0)
+2021-05-06 : 6 (+0)
+2021-05-07 : 7 (+0)
+2021-05-08 : 8 (+0)
+
+Group: Special
+2021-05-03 : 1 (+0)
+
+Total open slots for St. Joseph’s Health Centre : 32
+Change since last fetch for St. Joseph’s Health Centre : +0
+
+
+Clinic: West Park Healthcare Centre
+---------------------------------
+Group: Special
+2021-05-03 : 1 (+0)
+
+Total open slots for West Park Healthcare Centre : 1
+Change since last fetch for West Park Healthcare Centre : +0
+
+
+Clinic: Community Hub Place
+---------------------------------
+Total open slots for Community Hub Place : 0
+Change since last fetch for Community Hub Place : +0
+```
 As the script runs continuously it will be generating these json files, in future we can just rotate the fetched data into `last-run.json` file but for now it will be creating these json files for each fetch.
 
 To stop the script just exit using a keyboard interrupt `Ctrl + c` the script will overwrite the `data/last-run.json`
--- get_available_slots.py
+++ get_available_slots.py
@@ -23,15 +23,15 @@ def get_open_slots():
 
     response = requests.get(url = VACCINETO_API_SLOTS)
     full_data = response.json()
-    data = full_data['data']
+    data = full_data["data"]
     parsed_data = {}
-    parsed_data['date'] = datetime.now().strftime("%Y-%d-%mT%H:%M:%S")
+    parsed_data["date_fetched"] = datetime.now().strftime("%Y-%d-%mT%H:%M:%S")
+    parsed_data["data"] = {}
 
     for clinic in data:
-        clinic_name = data[clinic]['name']
         total_open_slots = 0
         clinic_data = {}
-        clinic_data["name"] = clinic_name
+        clinic_data["name"] = data[clinic]["name"]
         clinic_data["open_slots"] = {}
         clinic_open_slots = {}
         clinic_open_slots_group = {}
@@ -39,8 +39,8 @@ def get_open_slots():
         for group in data[clinic]['availabilities']:
             open_slots = 0
             clinic_open_slots_group_open_slots = {}
-            for slot in data[clinic]['availabilities'][group]:
-                open_slots = data[clinic]['availabilities'][group][slot]
+            for slot in data[clinic]["availabilities"][group]:
+                open_slots = data[clinic]["availabilities"][group][slot]
                 trimmed_datetime = slot.split("T")[0]
                 clinic_open_slots_group_open_slots[trimmed_datetime] = open_slots
                 total_open_slots += open_slots
@@ -48,52 +48,52 @@ def get_open_slots():
 
         clinic_data["open_slots"] = clinic_open_slots_group
         clinic_data["total_open_slots"] = total_open_slots
-        parsed_data[clinic] = clinic_data
+        parsed_data["data"][clinic] = clinic_data
 
     date_time_obj = datetime.now()
-    output_json_file = date_time_obj.strftime("%Y-%d-%m_%H-%M-%S") + '.json'
+    output_json_file = "{}.json".format(date_time_obj.strftime("%Y-%d-%m_%H-%M-%S"))
 
+    for clinic in parsed_data["data"]:
+        total_open_slots = parsed_data["data"][clinic]["total_open_slots"]
+        last_run_total_open_slots = last_run_json["data"][clinic]["total_open_slots"]
+        total_diff_since_last_run = total_open_slots - last_run_json["data"][clinic]["total_open_slots"] 
+        parsed_data["data"][clinic]["change_since_last_fetch"] = total_open_slots - last_run_total_open_slots
 
-    for data in parsed_data:
-        if (data != 'date'):
-            total_open_slots = parsed_data[data]['total_open_slots']
-            last_run_total_open_slots = last_run_json[data]['total_open_slots']
-            total_diff_since_last_run = total_open_slots - last_run_json[data]['total_open_slots'] 
-            parsed_data[data]['change_since_last_fetch'] = total_open_slots - last_run_total_open_slots
+        print("")
+        print("Clinic: {}".format(parsed_data["data"][clinic]["name"]))
+        print("---------------------------------")
 
-            print("")
-            print("Clinic: {}".format(parsed_data[data]['name']))
-            print("---------------------------------")
+        for group in parsed_data["data"][clinic]["open_slots"]:
+            group_summary_msg = "Group: {}\n".format(group)
+            are_slots_available = False
+            for date in parsed_data["data"][clinic]["open_slots"][group]:
+                available_slots = parsed_data["data"][clinic]["open_slots"][group][date]
+                if (available_slots > 0):
+                    are_slots_available = True
+                    diff_since_last_run_slot = available_slots - last_run_json["data"][clinic]["open_slots"][group][date]
+                    group_summary_msg += "{} : {} ({:+})\n".format(date, available_slots, diff_since_last_run_slot)
+            if are_slots_available:
+                print(group_summary_msg)
 
-            for group in parsed_data[data]['open_slots']:
-                group_summary_msg = "Group: {}\n".format(group)
-                are_slots_available = False
-                for date in parsed_data[data]['open_slots'][group]:
-                    available_slots = parsed_data[data]['open_slots'][group][date]
-                    if (available_slots > 0):
-                        are_slots_available = True
-                        diff_since_last_run_slot = available_slots - last_run_json[data]['open_slots'][group][date]
-                        group_summary_msg += "{} : {} ({:+})\n".format(date, available_slots, diff_since_last_run_slot)
-                if are_slots_available:
-                    print(group_summary_msg)
-
-            print("Total open slots for {} : {}".format(parsed_data[data]['name'], total_open_slots))
-            print("Change since last fetch for {} : {:+}".format(parsed_data[data]['name'], total_diff_since_last_run))
-            print("")
+        print("Total open slots for {} : {}".format(parsed_data["data"][clinic]["name"], total_open_slots))
+        print("Change since last fetch for {} : {:+}".format(parsed_data["data"][clinic]["name"], total_diff_since_last_run))
+        print("")
 
     with open(os.path.join(DATA_DIR, output_json_file), 'w') as output_json:
         json.dump(parsed_data, output_json, indent=4)
     LAST_RUN_JSON = output_json_file
 
+
+
 def on_exit_handler():
-    os.rename(os.path.join(DATA_DIR, LAST_RUN_JSON), os.path.join(DATA_DIR, 'last-run.json'))
+    os.rename(os.path.join(DATA_DIR, LAST_RUN_JSON), os.path.join(DATA_DIR, "last-run.json"))
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     global DATA_DIR
     global LAST_RUN_JSON
-    LAST_RUN_JSON = 'last-run.json'
+    LAST_RUN_JSON = "last-run.json"
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    DATA_DIR = ROOT_DIR + '/data/'
+    DATA_DIR = os.path.join(ROOT_DIR, "data")
 
     atexit.register(on_exit_handler)
     get_open_slots()
